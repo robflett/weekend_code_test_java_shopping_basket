7687e741f819bc56c9c7ad43d8933b109a6d5812
Rob <robflettcc@gmail.com>
2017-04-01 16:04:11 +0100

    Remove test added

--- app/src/test/java/com/codeclan/shopping_cart_java/BasketTest.java
+++ app/src/test/java/com/codeclan/shopping_cart_java/BasketTest.java
@@ -12,8 +12,9 @@ public class BasketTest {
     @Before
     public void before(){}
 
-    ArrayList basket = new ArrayList();
+
     Item item1 = new Item("Milk", 69, 2);
+    ArrayList basket = new ArrayList();
 
 
     @Test
@@ -27,6 +28,15 @@ public class BasketTest {
         basket.add(item1);
         basket.add(item2);
         assertEquals(2, basket.size());
+    }
+
+    @Test
+    public void testRemoveItem(){
+        Item item2 = new Item("Bread", 140, 1);
+        basket.add(item1);
+        basket.add(item2);
+        basket.remove(item2);
+        assertEquals(1, basket.size());
     }
 
 }
