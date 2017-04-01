ab77e6a86b3bbc257a526da230158a81b2eb1a8c
Rob <robflettcc@gmail.com>
2017-04-01 15:56:59 +0100

    Refactored to use ArrayList

--- app/src/main/java/com/codeclan/shopping_cart_java/Basket.java
+++ app/src/main/java/com/codeclan/shopping_cart_java/Basket.java
@@ -1,17 +1,26 @@
 package com.codeclan.shopping_cart_java;
 
 
+import java.util.ArrayList;
+
 public class Basket {
 
     private double totalPrice;
     private int itemCount;
-    private static Item[] basket = new Item[10];
+    private ArrayList<Item> basket;
 
-    public Basket(double totalPrice, int itemCount) {
+
+
+    public Basket(double totalPrice, int itemCount, ArrayList<Item> basket) {
         this.totalPrice = totalPrice;
         this.itemCount = itemCount;
+        this.basket = basket;
     }
 
+
+    public void addItem(Item item){
+        basket.add(item);
+    }
 
 
 }
--- app/src/test/java/com/codeclan/shopping_cart_java/BasketTest.java
+++ app/src/test/java/com/codeclan/shopping_cart_java/BasketTest.java
@@ -3,6 +3,8 @@ package com.codeclan.shopping_cart_java;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class BasketTest {
@@ -10,10 +12,21 @@ public class BasketTest {
     @Before
     public void before(){}
 
-    Item[] basket = new Item[10];;
+//    Item[] basket = new Item[10];
+    ArrayList basket = new ArrayList();
+    Item item1 = new Item("Milk", 69, 2);
+
 
     @Test
     public void testGetArrayLength()  {
-        assertEquals(10, basket.length);
+        basket.add(item1);
+        assertEquals(1, basket.size());
     }
+
+//    @Test
+//    public void testAddtoArrayList(){
+//        basket.
+//
+//    }
+
 }
