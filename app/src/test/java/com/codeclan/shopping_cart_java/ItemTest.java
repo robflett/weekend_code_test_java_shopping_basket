74bb339b83e806ff78913a2dd1fa9d80cfadf68c
Rob <robflettcc@gmail.com>
2017-04-01 15:15:04 +0100

    new test passing

--- app/src/test/java/com/codeclan/shopping_cart_java/ItemTest.java
+++ app/src/test/java/com/codeclan/shopping_cart_java/ItemTest.java
@@ -1,5 +1,6 @@
 package com.codeclan.shopping_cart_java;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,12 +8,22 @@ import static org.junit.Assert.*;
 
 public class ItemTest {
 
-    @Test
-    public void testGetItem_Name()  {
+    @Before
+    public void before(){}
 
         Item item1 = new Item("Milk", 69, 2);
 
+
+    @Test
+    public void testGetItem_Name()  {
         assertEquals("Milk", item1.getItem_name());
     }
+
+    @Test
+    public void testGetPrice(){
+        assertEquals(69, item1.getPrice(), 0.001);
+    }
+
+    
 
 }
